c68a69aadca8dac8afd21bdc67b1c7ff0cd1331f
rjOwl <mostafayossef100@yahoo.com>
2022-09-03 02:05:51 +0200

    Adding psudocode comments

--- forest_fire_sensors_server/main.cpp
+++ forest_fire_sensors_server/main.cpp
@@ -6,11 +6,32 @@ using namespace std;
 
 int main()
 {
+
     TemperatureSensor temperatureSensor;
     PayloadReceiver payRec = PayloadReceiver();
     payRec.receiveSensorPayload();
 
-//    std::cout << temperatureSensor.generateTemperature(10,20) << std::endl;
+    std::cout << temperatureSensor.getParsedPayload() << std::endl;
     std::cout << payRec.getPayload() << std::endl;
     return 0;
 }
+
+    /**
+    Server logic:
+    -------------
+    clients should only access the server if the have the credintials
+    Thread1:
+        while server is up then:
+            it will listen to incoming connections
+            if new connection then:
+                if credentials valid then:
+                    add the client to the clientList
+                else:
+                    don't add
+    Thread2:
+    while server is up:
+        if there are connected clients then:
+            server will loop through them and
+            each 1 second server will send temperature to the clients
+    */
+
