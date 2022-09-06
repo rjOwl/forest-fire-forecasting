425e214147543057fb2e5bd45e6e4669f69c58d5
rjOwl <mostafayossef100@yahoo.com>
2022-09-05 18:26:30 -0700

    in progress

---
+++ forest_fire_sensors_server/src/ConsolLogger.cpp
@@ -0,0 +1,15 @@
+#include "ConsolLogger.h"
+#include <iostream>
+void logData(string data){
+    cout<<data<<"\n";
+}
+
+ConsolLogger::ConsolLogger()
+{
+    //ctor
+}
+
+ConsolLogger::~ConsolLogger()
+{
+    //dtor
+}
