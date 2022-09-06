bee162ffd61443d12533250e05d1b9cd657dc17a
rjOwl <mostafayossef100@yahoo.com>
2022-09-05 18:26:06 -0700

    in progress

--- forest_fire_sensors_server/src/LoggerFactory.cpp
+++ forest_fire_sensors_server/src/LoggerFactory.cpp
@@ -1,5 +1,13 @@
 #include "LoggerFactory.h"
 
+//ILogger getLogger(string env){
+//    if(env.compare("console")){
+//        ILogger console = new ConsolLogger();
+//        return console
+//    }
+//    else
+//}
+
 LoggerFactory::LoggerFactory()
 {
     //ctor
