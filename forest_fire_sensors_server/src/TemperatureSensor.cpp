9fd19230733bbcc78d7c995b398085d59647a514
rjOwl <mostafayossef100@yahoo.com>
2022-09-06 13:01:27 -0700

    Fixed temperature logic

--- forest_fire_sensors_server/src/TemperatureSensor.cpp
+++ forest_fire_sensors_server/src/TemperatureSensor.cpp
@@ -2,22 +2,24 @@
 #include "limits.h"
 #include <cstdlib>
 
+#include <iostream>
 
 string TemperatureSensor::getParsedPayload(){}
 
 
 void TemperatureSensor::parsePayload(string rawPayload) {
     size_t pos_start = 0, pos_end, delim_len = TemperatureSensor::delimiter.length();
+    vector<string> parsedPayload;
     string token;
 
     while ((pos_end = rawPayload.find (TemperatureSensor::delimiter, pos_start)) != string::npos) {
         token = rawPayload.substr (pos_start, pos_end - pos_start);
         pos_start = pos_end + delim_len;
-        TemperatureSensor::parsedPayload.push_back(token);
+        parsedPayload.push_back(token);
     }
-    TemperatureSensor::parsedPayload.push_back (rawPayload.substr (pos_start));
-    TemperatureSensor::timestamp = TemperatureSensor::parsedPayload[1];
-    TemperatureSensor::temp = TemperatureSensor::parsedPayload[2];
+    parsedPayload.push_back (rawPayload.substr (pos_start));
+    TemperatureSensor::timestamp = parsedPayload[1];
+    TemperatureSensor::temp = parsedPayload[2];
 }
 
 string TemperatureSensor::getTemp(){
@@ -28,28 +30,3 @@ string TemperatureSensor::getTimestamp(){
     return TemperatureSensor::timestamp;
 }
 
-
-/*
-TemperatureSensor::TemperatureSensor(){
-    temperatureCounter = 0;
-}
-*/
-/*
-float TemperatureSensor::getTemperature(){
-    return celsiusTemperature;
-}
-
-void TemperatureSensor::setTemperature(float newTemperaturReading){
-    if (temperatureCounter == INT_MAX){
-        celsiusTemperature = (newTemperaturReading + (celsiusTemperature)) /  2;
-        temperatureCounter = 2;
-    }else{
-        celsiusTemperature = (newTemperaturReading + (temperatureCounter * celsiusTemperature)) / temperatureCounter + 1;
-        temperatureCounter++;
-    }
-}
-
-float TemperatureSensor::generateTemperature(int minReng, int maxReng){
-    return minReng + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(maxReng-minReng)));
-}
-*/
