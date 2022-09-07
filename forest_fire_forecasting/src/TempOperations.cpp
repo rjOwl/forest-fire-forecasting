c3020a13bd537e5a9a68b306fe82a0ce96899f75
rjOwl <mostafayossef100@yahoo.com>
2022-09-07 07:00:56 -0700

    Adding methods implementation

--- forest_fire_forecasting/src/TempOperations.cpp
+++ forest_fire_forecasting/src/TempOperations.cpp
@@ -9,3 +9,27 @@ TempOperations::~TempOperations()
 {
     //dtor
 }
+
+
+void TempOperations::setTempSum(float temp){
+    TempOperations::temp_sum+=temp;
+}
+
+float TempOperations::getTempSum(){
+    return TempOperations::temp_sum;
+}
+
+float TempOperations::calcAvgTemp(){
+    return TempOperations::temp_sum / TempOperations::temp_count;
+}
+float TempOperations::calcAccumulatedTemp(){
+    return TempOperations::temp_count;
+}
+
+void TempOperations::setTempCount(){
+    TempOperations::temp_count+=1;
+}
+int TempOperations::getTempCount(){
+    return TempOperations::temp_count;
+}
+
