b0a4f7f66ede4aabcd1018a582f0f343e28471c9
rjOwl <mostafayossef100@yahoo.com>
2022-09-07 07:00:37 -0700

    Adding methods implementation

--- forest_fire_forecasting/src/TemperatureSensor.cpp
+++ forest_fire_forecasting/src/TemperatureSensor.cpp
@@ -1,5 +1,6 @@
 #include "TemperatureSensor.h"
-
+#include <iostream>
+using namespace std;
 TemperatureSensor::TemperatureSensor()
 {
     //ctor
@@ -8,4 +9,12 @@ TemperatureSensor::TemperatureSensor()
 TemperatureSensor::~TemperatureSensor()
 {
     //dtor
+}
+
+void TemperatureSensor::setCurrTemp(float currTemp){
+    TemperatureSensor::current_temp = currTemp;
+}
+
+float TemperatureSensor::getCurrTemp(){
+    return TemperatureSensor::current_temp;
 }
